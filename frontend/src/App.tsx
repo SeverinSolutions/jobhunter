3142bfdf29e47b3da9a0220de727b5d1d3a0c809
sparkweaver <lundmark.johan.91@gmail.com>
2025-06-02 23:28:26 +0200

    remove: old styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
